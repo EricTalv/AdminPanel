0aeba21d4ee0eddadf3f685199041c0d99acd050
EricTalv <eric.talviste@tptlive.ee>
2019-01-30 19:59:45 +0200

    Tested MongoDB, Im lost here

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
-var express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var app = express();
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-var http = require('http').Server(app);
+// Database Name
+const dbName = 'myproject';
 
-var io = require('socket.io')(http);
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+// Use connect method to connect to the server
+MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertDocuments(db, function() {
+    client.close();
+  });
 });
 
-app.use("/static", express.static('./static/'));
-
-
-//We make the http server listen on port 3000.
-http.listen(3000, function() {
-    console.log('listening on *:3000');
-});
