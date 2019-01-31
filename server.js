6899a7f4c0a39d203b313de88a051d3c8e41e53c
EricTalv <eric.talviste@tptlive.ee>
2019-01-31 23:02:51 +0200

    Setting up mySQL Properties and launch options

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var express = require('mysql');
+
+var connect = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'admin',
+      database: 'sampleDB'
+});
+
