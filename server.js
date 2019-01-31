7e6f3836b00f6d73b7e00cea0812f11a495dc4bb
EricTalv <eric.talviste@tptlive.ee>
2019-01-31 23:10:25 +0200

    First Tests For SQL

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
-var express = require('mysql');
+var mysql = require('mysql');
+var app = express();
 
-var connect = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'admin',
-      database: 'sampleDB'
+//Setup Database connection properties 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'sampleDB'
 });
 
+//Establish connection
+connection.connect(function(error) {
+    //Check for connection issues
+    if (!!error) {
+        console.log('Error: ' + error);
+    } else {
+        console.log('Connection established.');
+    };
+});
+
+//Send data to app
+app.get('/', function(req, resp) {
+    //Send a MYSQL Query to ->sampleDataBase
+    connection.query("SELECT * FROM sampleDB", function(error, rows, fields) {
+        //Callback when query has been done
+        //Check for query errors
+        if (!!error) {
+            console.log('Error: ' + error);
+        } else {
+            console.log('Query Was Succesful');
+        };
+    });
+});
+
+app.listen(3000);
