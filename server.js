58488f29e45886116f4260cabb336b336cfc71e9
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-01 16:43:36 +0200

    Folder Restructuring, client panels go into view and admin panels to admin

--- server.js
+++ server.js
@@ -7,13 +7,34 @@ var app = express();
 //Establish connection to MySQL
 
 //Send a GET-> request to server
-//To retrieve data from source
+//To retrieve data from source 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');    
+    //give client panel path-> Index.html
+    res.sendFile(__dirname + '/view/index.html');    
 });
 
 //Use static-folder for static files
 app.use('/static/', express.static('./static/.'));
+
+//Login check 
+function requireLogin(req,res,next) {
+    //Check if user session is logged in
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+//Apply the requireLogin middleware to
+//all admin routes
+app.all('/admin/*', requireLogin, function (req,res,next) {
+    next();
+});
+
+app.get('/admin', function (req, res) {
+    res.send('Admin Granted');
+})
 
 //Start listening 
 app.listen(3000, function(){
