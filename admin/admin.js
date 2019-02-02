85c3c9af5bc08a8e526aac85c5302f3ffdef5bf7
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-02 12:35:09 +0200

    Last commit

---
+++ admin/admin.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Admin Panel</title>
+		<link rel="stylesheet" href="admin.css">
+	</head>
+	<body>
+		Welcome To The Admin Panel.
+	</html>
--- server.js
+++ server.js
@@ -18,21 +18,38 @@ app.use('/static/', express.static('./static/.'));
 
 //Upon entering /admin route
 app.get('/admin', function (req, res) {
-    res.sendFile(__dirname + '/view/index.html');        
+    // res.sendFile(__dirname + '/admin/admin.html');       
+     logger(req, res);
 });
-
 
 //Start listening 
 app.listen(3000, function(){
     console.log('App Listening on Port:3000');
 });
 
+function keyAuth(req,res,next) {
+    /*
+     1. Entering /admin route
+    1.1. Run keyAuth middleware
+     1.1.1. keyAuth middleware should check for an admin token
+      1.1.1.1. IF admin token doesnt exist its an automatic 
+                un-authorization
+     1.1.2. if kA middleware finds admin token it has to 
+     compare it to the admin_database, admin_token field.
+     1.1.2. if its correct it must also check session
+     key and compare it to admin_database session_key field.
+      1.1.2.1 if session_key is false go to un-authorized
+     1.1.3 if both admin_Token and session_key are correct
+     redirect client to /admin access granted.
+    */
+
+    
+}
 
 
 /*
 
 >--Admin authentication Architecture--<
-
 
 CLIENT Panel ->
 -> Request ADMIN Panel -> /admin
@@ -40,6 +57,7 @@ CLIENT Panel ->
  session if login has occured
 
  How does checking work?
+
  
  When the client goes to /login
  and logs in, the data is then checked
@@ -95,6 +113,5 @@ DATABASE - Store whole Website data
         - Admistrative CRUD functions for Client
         
 HASH ADMIN DATA IN DB
+*/
 
-
-*\
