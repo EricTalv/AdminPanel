f6b2652f4db58d18cd6da819090f21ee905db033
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 15:16:36 +0200

    Starting to work on models

---
+++ models/index.js
@@ -0,0 +1,12 @@
+/*
+
+Here we will instatniate database tables 
+
+*/
+
+/* Requiring Modules */
+var fs = require('fs'); // FS- for working with file-systems on PC
+var path = require('path');
+var sequelize = require('sequelize'); // Sequelize as DB ORM
+var lodash = require('lodash');
+
