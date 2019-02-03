8ca584fdb67f5bdc1357618397c0cd7d564114d6
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 15:23:56 +0200

    Sequelize ORM

--- models/index.js
+++ models/index.js
@@ -6,7 +6,9 @@ Here we will instatniate database tables
 
 /* Requiring Modules */
 var fs = require('fs'); // FS- for working with file-systems on PC
-var path = require('path');
-var sequelize = require('sequelize'); // Sequelize as DB ORM
-var lodash = require('lodash');
+var path = require('path'); // for working with file/dir paths
+var mSequelize = require('sequelize'); // Sequelize as DB ORM
+var lodash = require('lodash'); // LIB for better arrays and more
 
+// connect sequelize to DB
+var sequelize = new mSequelize('sampleDB', 'root', null); //DB USR PW
