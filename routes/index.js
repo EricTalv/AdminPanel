c7c4e2421c042c98fbf363756e2c74388896c4ba
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 19:24:57 +0200

    More Routing2

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,37 @@
+module.exports = function(app, passport) {
 
+    // GET The HOME-page
+    app.get('/', function(req, res, next) {
+        res.render('index', {
+            title: 'Index',
+            message: 'Index Page'
+        });
+    });
+
+    // GET The LOGIN-page
+    app.get('/login', function(req, res, next) {
+        res.render('login', {
+            title: 'Login',
+            message: 'Login Page',
+            flash_message: { req.flash('loginMessage')}
+        });
+    });
+
+    // Process the login form
+    app.post('/login', passport.authenticat('local', {
+    	successRedirect: '/success',
+    	failureRedirect: '/login',
+    	failureFlash: true 
+    }),
+    function (req, res) {
+     	console.log('Hi');
+     	res.redirect('/');
+     } 
+
+
+
+    );
+};
 
 
 
