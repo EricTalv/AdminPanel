2a68aa40b114643c0038189edc45df9e2680ec9d
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 18:36:07 +0200

    Running first test

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ var session = require('express-session');
 // var user = require('./routes/user');
 // var home = require('./routes/home');
 // var application = require('./routes/application');
-// var passportConfig = require('./config/passport');
+var {
+    userResponse,
+    validateUser,
+    secret
+} = require('./config/config');
+var passportConfig = require('./config/passport');
 
 // Set app Listening port 
 var port = process.env.PORT || 3000;
@@ -36,63 +41,62 @@ app.set('view engine', 'pug');
 // Set view path
 app.set('views', path.join(__dirname, 'views'));
 
-/* ~Parsers~ */ 
+/* ~Parsers~ */
 // Use urlencoded to deal with incoming Requests as Strings/arrays
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 // Parse incoming http-form data
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 // Parse cookie data
-// app.use(cookieParser());
+app.use(cookieParser());
+
+//
+passportConfig(passport);
 
 /* ~Passport And Session Properties~ */
 // Session 
 app.use(session({
-  // secret key for encrypting cookies
-   secret: 'withaspiritsoundsystem',
-  // storing sessions
-   resave: false,
-  // forces UnInitializedSessions to be saved in store 
-   saveUninitialized: true,
-  // Cookie should be set true on HTTPS
-   cookie: { secure: false } 
+    // secret key for encrypting cookies
+    secret: 'withaspiritsoundsystem',
+    // storing sessions
+    resave: false,
+    // forces UnInitializedSessions to be saved in store 
+    saveUninitialized: false,
+    // Cookie should be set true on HTTPS
+    cookie: {
+        secure: false
+    }
 }));
 
-// store session data
-
-
-
 // Initialize Passport
-// app.use(passport.initialize());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 // Route to homepage 
 app.use('/', index);
 
-// Create database Tables
-database 
-        .sequelize
-        //.sync()
-        .complete(function (error) {
-            if (error) {
-                  console.error(error.stack);
-                  res.status(500).send('[500]Something went wrong!');  
-            } else {
-              http.createServer(app).listen(app.get('port'), function() {
-                console.log('Listening: ' + app.get('port'));
-              });
-            }
-        });
+//
+app.post('/login', passport.authenticate('local'), userResponse);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  return res.json({status: 'success' });
+});
 
 // Error Handeling
-app.use(function (error,req,res,next) {
+app.use(function(error, req, res, next) {
     console.error(error.stack);
-    res.status(500).send('[500]Something went wrong!');  
+    res.status(500).send('[500]Something went wrong!');
 });
 
 /* ~Open Server ports~ */
-// // Start listening 
-// app.listen(port, function(error){
-//     // Error Check 
-//     if (!error) {
-//       console.log('App Listening on Port: %s', port);
-//     } else { console.log(error); }
-// });
+// Start listening 
+app.listen(port, function(error){
+    // Error Check 
+    if (!error) {
+      console.log('App Listening on Port: %s', port);
+    } else { console.log(error); }
+});
