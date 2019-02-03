06a0ec5b31bf4d40528327a096c97d6b20724a7e
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 13:29:57 +0200

    Testing setup

--- server.js
+++ server.js
@@ -21,15 +21,18 @@ var port = process.env.PORT || 3000;
 // Initialize exrpess
 var app = express();
 
+/* ~Static Files~ */
 // store static files (css, etc.)
-app.use(express.static(path.join(__dirname, 'static')));
-//app.locals.basedir = path.join(__dirname, 'static');
+app.use(express.static(path.join(__dirname, 'public')));
+//app.locals.basedir = path.join(__dirname, 'poublic');
 
+/* ~View Engine~ */
 // view engine setup
 app.set('view engine', 'pug');
 // Set view path
 app.set('views', path.join(__dirname, 'views'));
 
+/* ~Parsers~ */ 
 // Use urlencoded to deal with incoming Requests as Strings/arrays
 app.use(express.urlencoded());
 // Parse incoming http-form data
@@ -37,11 +40,26 @@ app.use(express.bodyParser());
 // Parse cookie data
 app.use(express.cookieParser());
 
+/* ~Passport And Session Properties~ */
+// Session secret key for encrypting cookies
+app.use(express.session({ secret: 'withaspiritsoundsystem'} ));
+// Initialize Passport
+app.use(passport.Initialize());
+// Create a Router object <-???
+app.use(app.router);
 
+// Error handeling
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+};
 
+// Set homepage 
+app.get('/', routes.index);
 
+/* ~Open Server ports~ */
 // Start listening 
 app.listen(port, function(error){
+    // Error Check 
     if (!error) {
       console.log('App Listening on Port: %s', port);
     } else { console.log(error); }
