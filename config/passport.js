451c0d5ce10dad235b90347b1b9ea8d518051f12
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-03 18:08:10 +0200

    Passport and MySQL init

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,31 @@ Passport and database connections
 
 /* ~Require Modules~*/
 var LocalStrategy = require('passport-local').Strategy; // Load passport strategy
-
+var mysql = require('mysql'); 
 
 /* ~MySQL Properties~ */
+//Create connection with MYSQL
+var connection = mysql.createConnection({ 
+	host: 'localhost',
+	user: 'root',
+	password: ''
+});
+
+// Set database
+connection.query('USE admin_auth');
+
+module.exports = function (passport) {
+	/* Passport Session Setuo */
+
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		connection.query("SELECT * FROM admin_data WHERE id = "
+			 			 	+ id, function (err, rows) {
+			 			 		done(err, rows[0]);
+			 			 	});
+	});
+	
+}
