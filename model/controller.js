182bac91d46be06e98ca1571175b4291fbce158e
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-08 15:48:01 +0200

    Database reconfiguration

--- model/controller.js
+++ model/controller.js
@@ -1,8 +1,11 @@
 var mysql = require('mysql'); // MySQL
 var dbconfig = require('./config/database'); // Load database-configuration
 
+// Create DB && MySQL Connection
+var connection = mysql.createConnection(dbconfig.connection);
 
-
+// Set DataBase To use
+connection.query('USE ' + dbconfig.content_db);
 
 module.exports = function (admin) {
 	
