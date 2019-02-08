e77f36ed00bfa619608ae14975905d74f12a04ea
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-08 16:58:02 +0200

    Passing data to indexer

--- model/controller.js
+++ model/controller.js
@@ -8,15 +8,5 @@ var connection = mysql.createConnection(dbconfig.connection);
 connection.query('USE ' + dbconfig.content_db);
 
 module.exports = function(admin) {
-
-	// Read all the data from the database
-    function (req, done) {
-        connection.query("SELECT * FROM content_data" function (err, rows) {
-        	if (err) {
-        		return done(err);
-        	} else {
-        		return done(rows[0]);
-        	}
-        });
-    }
+	console.log('test');
 }
