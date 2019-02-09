133fb965c2b7d527390b77bc2c50640010d3a3a7
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-09 10:57:56 +0200

    controllerjs

---
+++ model/controller.js
@@ -0,0 +1,17 @@
+/*
+
+Controller Database Connections
+
+*/
+
+/* ~Require Modules~*/
+var mysql = require('mysql'); // MySQL
+var dbconfig = require('./config/database'); // Load database-configuration
+
+/* ~MySQL Properties~ */
+//Create connection with MYSQL
+var connection = mysql.createConnection(dbconfig.connection); // Establish connection with database
+
+module.exports = function (controller) {
+	
+}
