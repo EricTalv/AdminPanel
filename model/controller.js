5ea3c936898a8f6a772fed1f74ac908139e85045
EricTalv <eric.talviste@tptlive.ee>
2019-02-23 22:02:28 +0200

    Tidying

--- model/controller.js
+++ model/controller.js
@@ -9,16 +9,17 @@ var mysql = require('mysql'); // MySQL
 var dbconfig = require('./config/database'); // Load database-configuration
 
 /* ~MySQL Properties~ */
-//Create connection with MySQL
+// Create connection with MySQL
 var connection = mysql.createConnection(dbconfig.connection); // Establish connection with database
 
+// Use database 
 connection.query('USE ' + dbconfig.admin_db);
 
 module.exports = {
     read: function (req, res) {
 
     	var readQuery;    	
-    	function onComplete(query_results){//readQuery = query_results;
+    	function onComplete(query_results){
     		readQuery = query_results;
     		res.send(readQuery);
     		console.log('Query Trasnferred');
@@ -33,8 +34,6 @@ module.exports = {
                 });
             });    		
     	};
-
     	query(onComplete);
-
     }
 };
