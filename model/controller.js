3cd6713d82424a2b9044ebafe332b166a47b7dcc
EricTalv <eric.talviste@tptlive.ee>
2019-02-24 21:22:52 +0200

    Testing promises

--- model/controller.js
+++ model/controller.js
@@ -18,22 +18,31 @@ connection.query('USE ' + dbconfig.database);
 module.exports = {
     read: function() {
 
-        var readQuery;
-        function onComplete(query_results) {
-            readQuery = query_results;
-            console.log('Query Trasnferred');
-            console.log(readQuery);
-        }
-
-        function query(whenDone) {
-            var query_results;
+        query(function sendQuery(){
             connection.query('SELECT * FROM content_data', function(error, results, fields) {
-                if (error) throw error;
-                results.forEach((results) => {
-                    whenDone(results);
-                });
-            });
-        };
-        query(onComplete);
+                if (error) return Promise.reject(error);
+                return Promise.resolve(results);
+            })
+        })
+
+
+        ////CALLBACK HELL   
+        // var readQuery;
+        // function onComplete(query_results) {
+        //     readQuery = query_results;
+        //     console.log('Query Trasnferred');
+        //     console.log(readQuery);
+        // }
+
+        // function query(whenDone) {
+        //     var query_results;
+        //     connection.query('SELECT * FROM content_data', function(error, results, fields) {
+        //         if (error) throw error;
+        //         results.forEach((results) => {
+        //             whenDone(results);
+        //         });
+        //     });
+        // };
+        // query(onComplete);
     }
 };
