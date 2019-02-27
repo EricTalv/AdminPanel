2a30b4047ac46ee9c3602144552e1e427933191d
EricTalv <eric.talviste@tptlive.ee>
2019-02-27 18:51:15 +0200

    bugs

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ module.exports = function(app, passport, controller) {
 
                 res.render('index', {
                     title: 'Index',
-                    message: db.data[0],
+                    message: db_data[0],
                     message2: 'Data Fetched!'
                 })
             })
